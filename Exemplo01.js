9c4b40bbba8d2e00738720a8e5b05881c9a1db68
Igor-Tschoeke <igorgabrieltsc@github.com>
2019-08-02 11:26:56 -0300

    desenvolvendo

--- Exemplo01.js
+++ Exemplo01.js
@@ -1,4 +1,18 @@
-$(function(){
+$(function () {
+    //var nome = '';
     $nome = '';
 
-})
+    $.ajax({
+        url:'https://viacep.com.br/ws/01001000/json/',
+        method: 'get',
+        success: function (data) { 
+            $Localidade = data.localidade;
+            $Logradouro = data.logradouro;
+
+            $('#Localidade').val($localidade);
+            $('#Logradouro').val($logradouro);
+        },
+        error: function (err) { }
+    })
+
+});
